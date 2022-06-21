ba45f404070ba8259d58ddd94c16e9e20b4aebeb
Sergio Peralta <seperalta88@gmail.com>
2022-06-21 07:17:57 -0300

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 
 edad = int(input("Ingrese su edad: "))
 
-print("Su edad es: ", edad)
+#Si la edad es mayor a 18 años
+
+if edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
+
+
+
+
