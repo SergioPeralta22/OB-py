fa7a6d5170dc3fcd4d1aad4caa8dd835356873c9
Sergio Peralta <seperalta88@gmail.com>
2022-06-21 07:30:16 -0300

    fourth commit

--- numbers.py
+++ numbers.py
@@ -3,5 +3,5 @@ numeroInicial = int(input("Ingrese un numero: "))
 numeroFinal = int(input("Ingrese un numero mayor que el anterior: "))
 
 
-for i in range(numeroInicial, numeroFinal + 1):
+for i in range(numeroInicial + 1, numeroFinal):
     print(i)
