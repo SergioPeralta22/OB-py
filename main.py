403738daf477c0356274d1f990285ba5f7bc5a5f
Sergio Peralta <seperalta88@gmail.com>
2022-06-21 07:27:19 -0300

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 edad = int(input("Ingrese su edad: "))
 
-#Si la edad es mayor a 18 años
 
 if edad >= 18:
     print("Usted es mayor de edad")
---
+++ numbers.py
@@ -0,0 +1,7 @@
+
+numeroInicial = int(input("Ingrese un numero: "))
+numeroFinal = int(input("Ingrese un numero mayor que el anterior: "))
+
+
+for i in range(numeroInicial, numeroFinal + 1):
+    print(i)
